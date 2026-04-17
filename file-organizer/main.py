53da61482bca76e6a6a5890e33f8843237cac20c
Abdul Rehman <abdulsbaheen@gmail.com>
2026-04-17 15:31:58 +0500

    file-organizer short description
    A Python-based file organizer that automatically sorts files into folders based on their type, helping keep directories clean and structured.

--- file-organizer/main.py
+++ file-organizer/main.py
@@ -1,1 +1,56 @@
+import os 
+import shutil
 
+folder_path = "downloads"
+
+file_type = {
+    "images": [".jpg",".png",".jpeg"],
+    "documents" :[".pdf","docx",".txt"],
+    "videos": [".mp4",".avi",".mkv"],
+    "music": [".mp3",".wav",".aac"]
+}
+
+# make the folder 
+def create_folders():
+   for folder in file_type.keys():
+       path = os.path.join (folder_path, folder)
+       if not os.path.exists(path):
+           os.mkdir(path)
+    # others files
+   others_path = os.path.join(folder_path, "others")
+   if not os.path.exists(others_path):
+       os.mkdir(others_path)
+       
+# get a file category
+def get_category(extension):
+    for category, extensions in file_type.items():
+        if extension.lower() in extensions:
+            return category
+    return "others"
+
+#organize files
+def organize_files():
+    for file in os.listdir(folder_path):
+        file_path = os.path.join(folder_path, file)
+        
+        if os.path.isdir(file_path):
+            continue
+        
+        _,extension = os.path.splitext(file)
+        
+        category = get_category(extension)
+        
+        dest_path = os.path.join(folder_path, category)
+        
+        try:
+            shutil.move(file_path, dest_path)
+            print(f"Moved: {file} to {category}")
+        except Exception as e:
+            print(f"Error occurred while moving {file}: {e}")
+            
+print("🗂 File Organizer Started...")
+
+create_folders()
+organize_files()
+
+print("✅ Files Organized Successfully!")         
